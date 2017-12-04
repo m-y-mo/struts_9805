d37eee0775eef46a2b9be0e7f371c3a784b4c389
Man Yue Mo <mmo@semmle.com>
2017-12-04 15:51:12 +0000

    Clone of Struts for CVE-9805

---
+++ core/src/test/java/com/opensymphony/xwork2/conversion/impl/StringConverterTest.java
@@ -0,0 +1,78 @@
+package com.opensymphony.xwork2.conversion.impl;
+
+import com.opensymphony.xwork2.ActionContext;
+import org.apache.struts2.StrutsInternalTestCase;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+public class StringConverterTest extends StrutsInternalTestCase {
+
+    public void testIntegerToStringConversionPL() throws Exception {
+        // given
+        StringConverter converter = new StringConverter();
+        Map<String, Object> context = new HashMap<>();
+        context.put(ActionContext.LOCALE, new Locale("pl", "PL"));
+
+        // when
+        Object value = converter.convertValue(context, null, null, null, 234, null);
+
+        // then
+        assertEquals("234", value);
+    }
+
+    public void testDoubleToStringConversionPL() throws Exception {
+        // given
+        StringConverter converter = new StringConverter();
+        Map<String, Object> context = new HashMap<>();
+        context.put(ActionContext.LOCALE, new Locale("pl", "PL"));
+
+        // when
+        Object value = converter.convertValue(context, null, null, null, 234.12, null);
+
+        // then
+        assertEquals("234,12", value);
+    }
+
+    public void testBigDecimalToStringConversionPL() throws Exception {
+        // given
+        StringConverter converter = new StringConverter();
+        Map<String, Object> context = new HashMap<>();
+        context.put(ActionContext.LOCALE, new Locale("pl", "PL"));
+
+        // when
+        Object value = converter.convertValue(context, null, null, null, BigDecimal.valueOf(234.12), null);
+
+        // then
+        assertEquals("234,12", value);
+    }
+
+    public void testStringArrayToStringConversion() {
+        // given
+        StringConverter converter = new StringConverter();
+        Map<String, Object> context = new HashMap<>();
+        context.put(ActionContext.LOCALE, new Locale("pl", "PL"));
+
+        // when
+        Object value = converter.convertValue(context, null, null, null, new String[] {"foo", "baz"}, null);
+
+        // then
+        assertEquals("foo, baz", value);
+   }
+
+    public void testArrayOfNullToStringConversion() {
+        // given
+        StringConverter converter = new StringConverter();
+        Map<String, Object> context = new HashMap<>();
+        context.put(ActionContext.LOCALE, new Locale("pl", "PL"));
+
+        // when
+        Object value = converter.convertValue(context, null, null, null, new String[] {null}, null);
+
+        // then
+        assertEquals("", value);
+   }
+
+}
